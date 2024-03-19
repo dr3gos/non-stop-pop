d0deb32d9e78faedcba16325dfa6e78e2f6473ab
dr3gos - macbook <for.my.games.email@gmail.com>
2024-03-19 21:55:13 +0200

    added a bit of a 'fade in'

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ for file in os.listdir("../non-stop-pop-audio-files"):
         if file != "test.wav":
             songfilenames.append(file)
 
+random.shuffle(songfilenames)
+
 foldernames = []
 
 # foldernames = os.listdir("../non-stop-pop-audio-files/gta5")
@@ -33,8 +35,23 @@ for file in os.listdir(f"../non-stop-pop-audio-files/gta5/{randomfolder}"):
 
 random.randint(0, len(songfilenames) - 1)
 
+
+
 for i in songfilenames:
-    combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{generalfilenames[random.randint(0, len(generalfilenames) - 1)]}")
-    combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")
+    randomfile = generalfilenames[random.randint(0, len(generalfilenames) - 1)]
+
+    djfile = AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}")
+    song = AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")
+    partofsong = song[:len(djfile)] - 20
+    overlayedpart = djfile.overlay(partofsong, position=0)
+
+    restofsong = song[len(djfile):]
+
+    combinedSongs += overlayedpart
+    combinedSongs += restofsong
+
+    # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}").overlay(AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[:len( AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}"))] - 30, position=0)
+    # # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[:len(randomfile)] - 30
+    # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[len(AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[len(AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}"))]):]
 
 combinedSongs.export("../non-stop-pop-audio-files/test.wav", format="wav")
