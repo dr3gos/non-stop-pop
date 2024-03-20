8d77ecb98cee2b4a1de7ba5e0677546f0874ba98
dr3gos - macbook <for.my.games.email@gmail.com>
2024-03-20 08:15:47 +0200

    alpha v0.0.1

--- main.py
+++ main.py
@@ -4,54 +4,52 @@ from pydub import AudioSegment
 
 combinedSongs = AudioSegment.empty()
 
+def getRandomPath():
+    foldernames = []
+    for file in os.listdir("../non-stop-pop-audio-files/gta5"):
+        if file != ".DS_Store":
+            foldernames.append(file)
+    generalfilenames = []
+    randomfolder = foldernames[random.randint(0, len(foldernames)-3)]
+    for file in os.listdir(f"../non-stop-pop-audio-files/gta5/{randomfolder}"):
+        if file.endswith(".wav"):
+            generalfilenames.append(file)
+    randomfile = generalfilenames[random.randint(0, len(generalfilenames) - 1)]
+    randomfilepath = f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}"
+    return randomfilepath
+
+
 songfilenames = []
 for file in os.listdir("../non-stop-pop-audio-files"):
     if file.endswith(".wav"):
         if file != "test.wav":
             songfilenames.append(file)
-
 random.shuffle(songfilenames)
-
-foldernames = []
-
-# foldernames = os.listdir("../non-stop-pop-audio-files/gta5")
-
-for file in os.listdir("../non-stop-pop-audio-files/gta5"):
-    # if file.endswith(".wav"):
-    if file != ".DS_Store":
-        foldernames.append(file)
-
-generalfilenames = []
-
-randomfolder = foldernames[random.randint(0, len(foldernames)-3)]
-
-for file in os.listdir(f"../non-stop-pop-audio-files/gta5/{randomfolder}"):
-    if file.endswith(".wav"):
-        generalfilenames.append(file)
-
-# print(os.listdir())
-
-# print(songfilenames)
-
-random.randint(0, len(songfilenames) - 1)
 
 
 
 for i in songfilenames:
-    randomfile = generalfilenames[random.randint(0, len(generalfilenames) - 1)]
 
-    djfile = AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}")
+    randomfile = getRandomPath()
+
+    djfile = AudioSegment.from_wav(randomfile)
     song = AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")
-    partofsong = song[:len(djfile)] - 20
-    overlayedpart = djfile.overlay(partofsong, position=0)
 
-    restofsong = song[len(djfile):]
+    
 
-    combinedSongs += overlayedpart
-    combinedSongs += restofsong
+    partofsong = song[:len(djfile)+500] - 20
+    djpartofsong = djfile[:len(djfile)]
+    restofsong = song[len(djfile)+500:]
 
-    # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}").overlay(AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[:len( AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}"))] - 30, position=0)
-    # # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[:len(randomfile)] - 30
-    # combinedSongs += AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[len(AudioSegment.from_wav(f"../non-stop-pop-audio-files/{i}")[len(AudioSegment.from_wav(f"../non-stop-pop-audio-files/gta5/{randomfolder}/{randomfile}"))]):]
+    song = partofsong.append(restofsong, crossfade=500)
+
+    finalsong = djfile.overlay(song, position=0)
+    finalsong += song[len(djfile):]
+
+    finalsong.detectsilence(min_silence_len=100, silence_thresh=-20, seek_step=1)
+
+    combinedSongs += finalsong
+    
+
 
 combinedSongs.export("../non-stop-pop-audio-files/test.wav", format="wav")
